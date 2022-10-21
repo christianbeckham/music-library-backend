59c2885ce7f2e32bb5b7696a45ddd3f8c07e93e0
Christian Beckham <urenacb22@gmail.com>
2022-10-21 14:40:45 -0500

    feat: added functionality to update with a PUT request method

--- songs/views.py
+++ songs/views.py
@@ -21,9 +21,14 @@ def songs_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def songs_details(request, pk):
     song = get_object_or_404(Song, pk=pk)
     if request.method == 'GET':
         serializer = SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
