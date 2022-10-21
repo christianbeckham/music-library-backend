c0bd94a13475b336ca94fd0e9e896dc725e1bd73
Christian Beckham <urenacb22@gmail.com>
2022-10-21 14:07:45 -0500

    feat: added functionality to get all songs with a GET request

--- songs/views.py
+++ songs/views.py
@@ -10,7 +10,11 @@ from .serializers import SongSerializer
 
 @api_view(['GET', 'POST'])
 def songs_list(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
         serializer = SongSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
