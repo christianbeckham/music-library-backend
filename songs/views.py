7033aeafb19a93318216aace880393904215e051
Christian Beckham <urenacb22@gmail.com>
2022-10-21 14:01:53 -0500

    feat: added functionality to add a song with a POST request

--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Song
+from .serializers import SongSerializer
 
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
-    pass
+    if request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
